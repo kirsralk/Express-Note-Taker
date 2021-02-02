b94d01c2311c51e3e34eb19b0fd151f72c56e9ae
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-02-02 08:32:40 -0600

    basic routes to get index and notes pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Dependencies
+// =================================================================
 var express = require("express");
 var path = require("path");
 
@@ -8,7 +9,27 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+
+// ROUTES
+// =================================================================
+
+// Basic route that sends user to the main 'index.html' page
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Sends user to the 'notes.html' page
+app.get("./public/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+
+
+
+
+
 // Start server to begin listening
+// =================================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 })
