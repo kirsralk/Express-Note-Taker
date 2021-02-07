a3272d6e0aae509b2bb61507c32b3216b68d54ee
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-02-07 17:28:44 -0600

    writing db.json correctly with hard coded test

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var bodyParser = require("body-parser");
 var util = require("util");
 
 // Set up Express
@@ -11,8 +10,6 @@ var app = express();
 var PORT = 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(bodyParser.json());
-
 
 // ROUTES
 // =================================================================
@@ -34,23 +31,47 @@ app.get("/api/notes", function (req, res) {
 
 // Receive a note to save on the request body, add it to db.json
 app.post("/api/notes", function(req, res) {
-  
-    var savedNotes = fs.readFileSync("./db/db.json", "utf8")
-    console.log("line 39..." + savedNotes);
 
-    var query = JSON.stringify(req.query);
-
-    fs.appendFile("db/db.json", "\n" + query, (err) => {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
         if (err) {
-            console.error(err);
-            return
+            console.log(err);
         } else {
-            console.log("\nFile Contents after append:\n",
-            fs.readFileSync("db/db.json", "utf8"));
+            const file = JSON.parse(data);
+            console.log("file=" + file);
+            file.push({"title":"line 44","text":"444444"});
+            const json = JSON.stringify(file);
+            console.log("json=" + json);
+
+        fs.writeFile("db/db.json", json, "utf8", (err) => {
+            if (err) {
+                console.error(err);
+                return
+            } else {
+                console.log("\nFile Contents after append:\n",
+                fs.readFileSync("db/db.json", "utf8"));
+            }
+        });
+            console.log("line 52 ran then returned");
+            return
+
         }
+
+        
     });
-        console.log("line 52 ran then returned");
-        return
+    // var savedNotes = fs.readFileSync("./db/db.json", "utf8")
+    // console.log("savedNotes var = " + savedNotes);
+    // //this is grabbing the note contents!
+
+    // var savedNotes2 = [savedNotes];
+    // console.log("savedNotes2 with savedNotes = " + savedNotes2);
+
+    // savedNotes2.push(req.query);
+
+    // console.log("savedNotes2 after pushing query =" + savedNotes2);
+
+    // savedNotesStr = JSON.stringify(savedNotes2);
+
+
 });
 
 
